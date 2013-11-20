809dac30c272c1e67e001f77f100af04ae56615f
Roy Liu <carsomyr@gmail.com>
2013-11-20 15:30:45 -0500

    Add Ember.js as a vendorized asset

--- lib/scalient/rails/engine.rb
+++ lib/scalient/rails/engine.rb
@@ -7,6 +7,13 @@ module Scalient
   module Rails
     class Engine < ::Rails::Engine
       isolate_namespace Scalient::Rails
+
+      initializer "scalient-rails.setup_vendor", :after => "ember_rails.setup_vendor", :group => :all do |app|
+        variant = app.config.ember.variant || (::Rails.env.production? ? :production : :development)
+
+        ember_path = Engine.root.join("vendor/assets/ember", variant.to_s)
+        app.assets.prepend_path(ember_path.to_s) if ember_path.exist?
+      end
     end
   end
 end
