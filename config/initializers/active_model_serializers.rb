860685975a26d7542fcfe386e9739209169fc578
Roy Liu <carsomyr@gmail.com>
2014-03-16 21:21:34 -0400

    Revert parts of the active_model_serializers gem's monkey patch to reflect upstream changes

--- config/initializers/active_model_serializers.rb
+++ config/initializers/active_model_serializers.rb
@@ -52,7 +52,7 @@ module ActiveModel
             end
 
             if hash.has_key?(key)
-              (hash[key] ||= []).concat(serialized_data || []).uniq!
+              hash[key].concat(serialized_data).uniq!
             else
               hash[key] = serialized_data
             end if !key.nil?
