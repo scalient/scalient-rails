5249e4679168438c143bb35b5563808162c8f139
Roy Liu <carsomyr@gmail.com>
2014-11-25 17:39:06 -0500

    Update the `active_model_serializers` monkey patch

--- config/initializers/active_model_serializers_monkey_patch.rb
+++ config/initializers/active_model_serializers_monkey_patch.rb
@@ -14,6 +14,8 @@
 # License for the specific language governing permissions and limitations under
 # the License.
 
+require "active_model/serializer"
+
 ActiveModel::Serializer.setup do |config|
   config.embed = :ids
   config.embed_in_root = true
@@ -21,24 +23,15 @@ end
 
 module ActiveModel
   class Serializer
-    # Monkey patch broken "has_one" association behavior when used with the "root" option, and add support for
-    # polymorphism.
+    # Add support for polymorphism.
     def embedded_in_root_associations
       associations = self.class._associations
       included_associations = filter(associations.keys)
       associations.each_with_object({}) do |(name, association), hash|
         if included_associations.include? name
           association_serializer = build_serializer(association)
-
-          # Two associations could share the same root key.
-          hash.merge! association_serializer.embedded_in_root_associations do |_, lhs, rhs|
-            if lhs.is_a?(Array) && rhs.is_a?(Array)
-              merged = lhs + rhs
-              merged.uniq!
-              merged
-            else
-              rhs
-            end
+          hash.merge!(association_serializer.embedded_in_root_associations) do |key, oldval, newval|
+            [newval, oldval].flatten.uniq!
           end
 
           if association.embed_in_root?
@@ -50,15 +43,24 @@ module ActiveModel
 
             if !association.polymorphic?
               key = association.root_key
-            else
-              key = object.send("#{association.name}_type").demodulize.underscore.pluralize
-              serialized_data = serialized_data.map { |data| data[data[:type]] }
-            end
 
-            if hash.has_key?(key)
-              hash[key].concat(serialized_data).uniq!
+              if hash.has_key?(key)
+                hash[key].concat(serialized_data).uniq!
+              else
+                hash[key] = serialized_data
+              end
             else
-              hash[key] = serialized_data
+              serialized_data.each do |datum|
+                type = datum[:type]
+                key = type.to_s.pluralize
+                datum = datum[type]
+
+                if hash.has_key?(key)
+                  hash[key].push(datum).uniq!
+                else
+                  hash[key] = [datum]
+                end
+              end
             end
           end
         end
