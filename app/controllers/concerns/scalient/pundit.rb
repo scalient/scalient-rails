132a499f3a7aecb396ddd0df25016ee0da351652
Roy Liu <carsomyr@gmail.com>
2019-05-06 17:24:39 -0400

    Update the `Scalient::Pundit` concern to detect and no act upon Devise sign-in path redirect loops

--- app/controllers/concerns/scalient/pundit.rb
+++ app/controllers/concerns/scalient/pundit.rb
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-# Copyright 2015 Roy Liu
+# Copyright 2015-2019 Roy Liu
 #
 # Licensed under the Apache License, Version 2.0 (the "License"); you may not
 # use this file except in compliance with the License. You may obtain a copy of
@@ -25,9 +25,24 @@ module Scalient
 
       def not_authorized
         referrer = request.referrer
+        is_self_redirect_loop = referrer && URI(request.original_url) == URI(referrer)
 
-        redirect_to referrer && URI(request.original_url) != URI(referrer) ? referrer : root_path,
-                    alert: "You are not authorized to perform this action."
+        if referrer && defined?(Devise)
+          is_sign_in_redirect_loop = !!Devise.mappings.find do |scope, _|
+            sign_in_helper = "new_#{scope.to_s}_session_url"
+
+            if respond_to?(sign_in_helper) && URI(referrer) == URI(send(sign_in_helper))
+              scope
+            else
+              nil
+            end
+          end
+        else
+          is_sign_in_redirect_loop = false
+        end
+
+        redirect_to referrer && !is_self_redirect_loop && !is_sign_in_redirect_loop ? referrer : root_path,
+            alert: "You are not authorized to perform this action."
       end
     end
   end
